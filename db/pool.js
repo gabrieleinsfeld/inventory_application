9a04f699983a673b57f35d41b04a9c7c3c0ded06
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-08-08 14:52:03 -0300

    A

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,13 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+// const { Pool } = require("pg");
+// require("dotenv").config();
 
-module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: 5432, // The default port
-});
+// module.exports = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: 5432, // The default port
+// });
 
 const { Pool } = require("pg");
 
